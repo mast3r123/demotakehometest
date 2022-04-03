12061eb4f37edcce531f04464c9f18c0df1cadd6
mast3r123 <abhishek.mast3r@gmail.com>
2022-04-02 20:30:26 -0400

    setup tabbar with controllers

---
+++ GitSample/GitSample/FavoritesListVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesListVC.swift
+//  GitSample
+//
+//  Created by master on 4/2/22.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ GitSample/GitSample/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  GitSample
+//
+//  Created by master on 4/2/22.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
