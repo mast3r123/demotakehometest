b2c2063be6ca725b110034d2ffdd6f563b8d711f
mast3r123 <abhishek.mast3r@gmail.com>
2022-04-17 20:40:22 -0400

    empty state view

--- GitSample/GitSample/Controllers/FollowerListVC.swift
+++ GitSample/GitSample/Controllers/FollowerListVC.swift
@@ -60,6 +60,13 @@ class FollowerListVC: UIViewController {
                     self.hasMoreFollowers = false
                 }
                 self.followers.append(contentsOf: followers)
+                if self.followers.isEmpty {
+                    let message = "The user doesn't have any followers, feel free to follow them"
+                    DispatchQueue.main.async {
+                        self.showEmptyStateView(with: message, in: self.view)
+                    }
+                    return
+                }
                 self.updateData()
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Test", message: error.rawValue, buttonTitle: "Ok")
