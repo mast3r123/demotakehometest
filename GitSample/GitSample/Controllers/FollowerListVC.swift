157f7dd654c6a43e6d3627c6d02d4786f8917d17
mast3r123 <abhishek.mast3r@gmail.com>
2022-04-18 20:45:57 -0400

    follower list vc and modal presentation

--- GitSample/GitSample/Controllers/FollowerListVC.swift
+++ GitSample/GitSample/Controllers/FollowerListVC.swift
@@ -20,6 +20,7 @@ class FollowerListVC: UIViewController {
     var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
     var page: Int = 1
     var hasMoreFollowers = true
+    var isSearching = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -117,16 +118,28 @@ extension FollowerListVC: UICollectionViewDelegate {
             getFollowers(for: username, page: page)
         }
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let activeArray = isSearching ? filteredFollowers : followers
+        let follower = activeArray[indexPath.item]
+        
+        let destVC = UserInfoVC()
+        destVC.username = follower.login
+        let nav = UINavigationController(rootViewController: destVC)
+        present(nav, animated: true)
+    }
 }
 
 extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate {
     func updateSearchResults(for searchController: UISearchController) {
         guard let filter = searchController.searchBar.text, !filter.isEmpty else { return }
+        isSearching = true
         filteredFollowers = followers.filter{ $0.login.lowercased().contains(filter.lowercased()) }
         updateData(on: filteredFollowers)
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        isSearching = false
         updateData(on: followers)
     }
 }
