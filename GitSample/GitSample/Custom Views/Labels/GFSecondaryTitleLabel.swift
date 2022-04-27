b01048006fb46eaa60616a03050737c57253edfc
mast3r123 <abhishek.mast3r@gmail.com>
2022-04-27 17:31:45 -0400

    header child vc

---
+++ GitSample/GitSample/Custom Views/Labels/GFSecondaryTitleLabel.swift
@@ -0,0 +1,36 @@
+//
+//  GFSecondaryTitleLabel.swift
+//  GitSample
+//
+//  Created by master on 4/19/22.
+//
+
+import UIKit
+
+class GFSecondaryTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(size: CGFloat) {
+        super.init(frame: .zero)
+        font = UIFont.systemFont(ofSize: size, weight: .medium)
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        font = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.90
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
---
+++ GitSample/GitSample/Custom Views/ViewControllers/GFUserInfoHeaderVC.swift
@@ -0,0 +1,20 @@
+//
+//  GFUserInfoHeaderVC.swift
+//  GitSample
+//
+//  Created by master on 4/19/22.
+//
+
+import UIKit
+
+class GFUserInfoHeaderVC: UIViewController {
+    
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
