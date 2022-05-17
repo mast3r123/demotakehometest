fb0d5da2c57d0af57b9522d1c3326bac0217c69f
mast3r123 <abhishek.mast3r@gmail.com>
2022-05-17 16:08:59 -0400

    Fetching user and add to favorites

--- GitSample/GitSample/Controllers/FollowerListVC.swift
+++ GitSample/GitSample/Controllers/FollowerListVC.swift
@@ -113,6 +113,26 @@ class FollowerListVC: UIViewController {
     
     @objc func addButtonTapped() {
         
+        showLoadingView()
+        NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+            guard let self = self else { return }
+            self.dismissLoadingView()
+            switch result {
+            case .success(let user):
+                let favorite = Follower(login: user.login, avatarUrl: user.avatarUrl)
+                PersistenceManage.updateWith(favorite: favorite, actionType: .add) { [weak self] error in
+                    guard let self = self else { return }
+                    guard let error = error else {
+                        self.presentGFAlertOnMainThread(title: "Success", message: "Succesfully added to favorites 🎉", buttonTitle: "Ok")
+                        return
+                    }
+                    self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+                }
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
+        
     }
 }
 
